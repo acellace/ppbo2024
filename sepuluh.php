bced7339cd996fd0f7d0ff2f81b709d5e4522cee
acellace <h1101231006@student.untan.ac.id>
2024-09-18 21:27:30 +0700

    Praktikum 6D - Menggunakan Autoload dengan Composer

--- sepuluh.php
+++ sepuluh.php
@@ -1,9 +1,6 @@
 <?php
 
-
-require_once 'App/Admin/HomePage.php';
-require_once 'App/Admin/ManajemenArtikel.php';
-
+require_once 'vendor/autoload.php';
 
 use App\Admin\HomePage;
 use App\Admin\ManajemenArtikel;
@@ -11,7 +8,6 @@ use App\Admin\ManajemenArtikel;
 
 $hp = new HomePage();
 $hp->tampil();
-
 
 $ma = new ManajemenArtikel();
 $ma->tambah();
