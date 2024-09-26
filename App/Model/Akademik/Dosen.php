c6b1b6e5212889d1c41f8f5f028ed94373f08297
acellace <h1101231006@student.untan.ac.id>
2024-09-26 19:59:22 +0700

    Praktikum 6Cx - Implementasi Enkapsulasi

--- App/Model/Akademik/Dosen.php
+++ App/Model/Akademik/Dosen.php
@@ -5,7 +5,7 @@ namespace App\Model\Akademik;
 use App\Model\Akademik\Pegawai;
 
 class Dosen extends Pegawai {
-    public $nidn;
+    private $nidn;
     
     public function mengajar() : void {
         echo "{$this->nama} sedang mengajar perkuliahan";
--- sebelas.php
+++ sebelas.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 
 use App\Model\Akademik\Dosen;
 
-$dian = new Dosen();
+$dian =  new Dosen();
 $dian->nama = 'Dian Prawira';
 $dian->nip = '198411132015041001';
 $dian->setNoHp(62111111);
